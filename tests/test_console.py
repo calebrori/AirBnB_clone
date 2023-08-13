20a9fd543b1ee9080b4461bdd6336634d0ae522f
calebrori <32507245+calebrori@users.noreply.github.com>
2023-08-13 16:20:39 +0300

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,6 +20,7 @@ from models.engine.file_storage import FileStorage
 from console import HBNBCommand
 from io import StringIO
 
+
 class TestHBNBCommand_prompting(unittest.TestCase):
     """Unittests for testing prompting of the HBNB command interpreter."""
 
